200c8a8ae0204247daa24645dcc2212a71c873e1
ANNIE <anniekatelyntang@gmail.com>
2017-12-04 15:53:44 -0500

    changed static_url

--- bdrsite/settings.py
+++ bdrsite/settings.py
@@ -13,13 +13,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # /home/bluedevilresistome/bdrsite
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)) # /home/bluedevilresistome/bdrsite/bdrsite
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static') # /home/bluedevilresistome/bdrsite/bdrsite/static
 
-print(BASE_DIR)
-print(PROJECT_DIR)
-print(STATIC_ROOT)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -128,4 +125,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'home/bluedevilresistome/bdrsite/bdrsite/static/'
