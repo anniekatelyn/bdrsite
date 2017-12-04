824b2002a3116f004bd1435b26b0f8b0bee9c7d1
ANNIE <anniekatelyntang@gmail.com>
2017-12-04 16:00:21 -0500

    settings.py

--- bdrsite/settings.py
+++ bdrsite/settings.py
@@ -125,4 +125,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/home/bluedevilresistome/bdrsite/bdrsite/static/'
+STATIC_URL = '/static/'
